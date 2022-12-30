273cab0b88786f2bc15c10d52e2ac8c1ba27a587
PSheon <pauljiang61020@gmail.com>
2022-12-30 15:27:03 +0800

    feat: added routing function to logo.

--- src/layouts/components/AppBrand.tsx
+++ src/layouts/components/AppBrand.tsx
@@ -1,10 +1,15 @@
+// ** Next Import
+import Link from 'next/link'
+
 // ** MUI Imports
 import Box from '@mui/material/Box'
 
 const AppBrand = () => {
   return (
     <Box sx={{ display: 'flex', alignItems: 'center' }}>
-      <img src='/images/logos/logo.png' alt='logo' width='35' height='15' />
+      <Link href='/' passHref>
+        <img src='/images/logos/logo.png' alt='logo' width='35' height='15' />
+      </Link>
     </Box>
   )
 }
