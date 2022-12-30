98e6be11c63e5b67599310f53e418c5ac732b9c3
PSheon <pauljiang61020@gmail.com>
2022-12-30 16:35:24 +0800

    feat: updated home page layout.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,25 @@
 // ** MUI Imports
-import Card from '@mui/material/Card'
-import Grid from '@mui/material/Grid'
-import Typography from '@mui/material/Typography'
-import CardHeader from '@mui/material/CardHeader'
-import CardContent from '@mui/material/CardContent'
+import { styled } from '@mui/material/styles'
+import Box, { BoxProps } from '@mui/material/Box'
 
-const Home = () => {
+// ** Components Imports
+import SearchSection from 'src/views/home/search-section'
+import FollowerListSection from 'src/views/home/follower-list-section'
+
+// ** Styled RootBox component
+const StyledRootBox = styled(Box)<BoxProps>(({ theme }) => ({
+  display: 'flex',
+  width: '100%',
+  margin: theme.spacing(-6)
+}))
+
+const HomePage = () => {
   return (
-    <Grid container spacing={6}>
-      <Grid item xs={12}>
-        <Card>
-          <CardHeader title='Kick start your project 🚀'></CardHeader>
-          <CardContent>
-            <Typography sx={{ mb: 2 }}>All the best for your new project.</Typography>
-            <Typography>
-              Please make sure to read our Template Documentation to understand where to go from here and how to use our
-              template.
-            </Typography>
-          </CardContent>
-        </Card>
-      </Grid>
-      <Grid item xs={12}>
-        <Card>
-          <CardHeader title='ACL and JWT 🔒'></CardHeader>
-          <CardContent>
-            <Typography sx={{ mb: 2 }}>
-              Access Control (ACL) and Authentication (JWT) are the two main security features of our template and are
-              implemented in the starter-kit as well.
-            </Typography>
-            <Typography>Please read our Authentication and ACL Documentations to get more out of them.</Typography>
-          </CardContent>
-        </Card>
-      </Grid>
-    </Grid>
+    <StyledRootBox>
+      <SearchSection />
+      <FollowerListSection />
+    </StyledRootBox>
   )
 }
 
-export default Home
+export default HomePage
